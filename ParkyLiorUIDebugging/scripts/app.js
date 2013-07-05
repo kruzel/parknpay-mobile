4f70fa24522d5c8736b8be986a089158b4672cfd
doarzivli1 junk <doarzivli1@gmail.com>
2013-07-05 14:44:54 -0500

    Changes made by doarzivli1 junk (doarzivli1@gmail.com).
    partial fix of the default values after restart - the regions still need to update the scroller - need to parse the value kept in the non volatile storage

--- ParkyLiorUIDebugging/scripts/app.js
+++ ParkyLiorUIDebugging/scripts/app.js
@@ -254,6 +254,21 @@ function closeModalViewSignUp()
 	});
     
 }
+$('#scanview').click(function() 
+            {
+ 				window.plugins.barcodeScanner.scan(function(result) {
+					alert("We got a barcode\n" +
+						  "Result: " + result.text + "\n" +
+						  "Format: " + result.format + "\n" +
+						  "Cancelled: " + result.cancelled);
+				}, function(error) {
+					alert("Scanning failed: " + error);
+				}
+				);
+
+				return false;
+			});
+
 
 
 //////////////////////////
