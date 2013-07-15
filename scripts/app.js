b136bf69b297fefb5fff440029c3089bd192573a
lior <alior101@gmail.com>
2013-07-16 00:28:37 +0300

    camera is working on mobile - still dont knwo what's going to the server

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,77 @@
 // JavaScript Document
 
-if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) 
+// GLOBALS
+var _serverApi;
+var user_data_str;
+var user_data;
+var ParkingActive;
+
+function OnDocumentReady() 
 {
+	_serverApi = new serverApi();
+
+	user_data_str = window.localStorage.getItem("user_data");
+	user_data = null;
+	if(user_data_str!=null) {
+	    user_data = JSON.parse(user_data_str);
+	}
+    
+    // are we running in native app or in browser?
+    window.isphone = false;
+    
+	if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) 
+	{
+		window.isphone = true;
+	} 
+
+
+    if(window.isphone) 
+    {
         document.addEventListener("deviceready", onDeviceReady, false);
-} 
-else 
-{
-	onDeviceReady();
+    } 
+    else 
+    {
+        onDeviceReady();
+    }
 }
+
+function onDeviceReadyCamera() {
+    
+    //alert('onDeviceReadyCamera');
+    cameraApp = new cameraApp();
+    cameraApp.run();
+}
+
 
 
 // PhoneGap is ready
 function onDeviceReady() 
 {
-    console.log("onDeviceReady");
-    onDeviceReadyCamera();
-    getLocation();
+	console.log("onDeviceReady");
 	if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) 
 	{
+		onDeviceReadyCamera();
+		getLocation();
     		navigator.splashscreen.hide();
 	}
-}
+	else
+	{
+		$('#smallImage').click(function() 
+		{
+			console.log("desktop-click!!");
+		        var smallImage = document.getElementById('smallImage');
+			smallImage.style.display = 'block';
+			// Show the captured photo.
+			//smallImage.src = ;
+			return false;
+		});
+	}
 
-var _serverApi = new serverApi();
+} 
+    
 
-var user_data_str = window.localStorage.getItem("user_data");
-var user_data = null;
-if(user_data_str!=null) {
-    user_data = JSON.parse(user_data_str);
-}
-
-
-function id(element) {
+function id(element) 
+{
     return document.getElementById(element);
 }
 
@@ -39,17 +79,311 @@ function id(element) {
 // ===================// 
 // camera stuff       //  
 // ================== //
+        
 
+     
 
-function onDeviceReadyCamera() 
+function cameraApp(){}
+
+cameraApp.prototype =
 {
+        
+    _pictureSource: null,
+    _destinationType: null,
 
-    cameraApp = new cameraApp();
-    cameraApp.run();
+        
+    run: function()
+    {
+      
+        var that=this;
+        console.log("cameraApp.run!!");
+        
+	if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) 
+	{
+	    that._pictureSource = navigator.camera.PictureSourceType;
+	    that._destinationType = navigator.camera.DestinationType;
+	    
+		$('#smallImage').click(function() 
+		{
+			console.log("click!!");
+			//alert('bubu');
+			cameraApp._capturePhoto.apply(that,arguments);
+			return false;
+		});
+	}
+	else
+	{
+
+	}
+          
+    },
+     _capturePhoto: function() {
+        var that = this;
+        /* alert("YEAYEA!!"); */
+        /* Take picture using device camera and retrieve image as base64-encoded string. */
+        navigator.camera.getPicture(function(){
+            that._onPhotoDataSuccess.apply(that,arguments);
+        },function(){
+            that._onFail.apply(that,arguments);
+        },{
+            quality: 50,
+            destinationType: that._destinationType.DATA_URL
+        });
+    },
+   /*
+    _capturePhoto: function() {
+        var that = this;
+        console.log("YEAYEA!!"); 
+        // Take picture using device camera and retrieve image as base64-encoded string.
+        navigator.camera.getPicture(function()
+	{
+            that._onPhotoDataSuccess.apply(that,arguments);
+        },function(){
+            that._onFail.apply(that,arguments);
+        },{
+            quality: 50,
+            destinationType: that._destinationType.DATA_URL
+        });
+    },
+    */
+    _capturePhotoEdit: function() {
+        var that = this;
+        // Take picture using device camera, allow edit, and retrieve image as base64-encoded string. 
+        // The allowEdit property has no effect on Android devices.
+        navigator.camera.getPicture(function(){
+            that._onPhotoDataSuccess.apply(that,arguments);
+        }, function(){
+            that._onFail.apply(that,arguments);
+        }, {
+            quality: 20, allowEdit: true,
+            destinationType: cameraApp._destinationType.DATA_URL
+        });
+    },
+    
+    _getPhotoFromLibrary: function() {
+        var that= this;
+        // On Android devices, pictureSource.PHOTOLIBRARY and
+        // pictureSource.SAVEDPHOTOALBUM display the same photo album.
+        that._getPhoto(that._pictureSource.PHOTOLIBRARY);         
+    },
+    
+    _getPhotoFromAlbum: function() {
+        var that= this;
+        // On Android devices, pictureSource.PHOTOLIBRARY and
+        // pictureSource.SAVEDPHOTOALBUM display the same photo album.
+        that._getPhoto(that._pictureSource.SAVEDPHOTOALBUM)
+    },
+    
+    _getPhoto: function(source) {
+        var that = this;
+        // Retrieve image file location from specified source.
+        navigator.camera.getPicture(function(){
+            that._onPhotoURISuccess.apply(that,arguments);
+        }, function(){
+            cameraApp._onFail.apply(that,arguments);
+        }, {
+            quality: 50,
+            destinationType: cameraApp._destinationType.FILE_URI,
+            sourceType: source
+        });
+    },
+    
+    _onPhotoDataSuccess: function(imageData) {
+        var smallImage = document.getElementById('smallImage');
+        smallImage.style.display = 'block';
+    
+        // Show the captured photo.
+        smallImage.src = "data:image/jpeg;base64," + imageData;
+    },
+    
+    _onPhotoURISuccess: function(imageURI) {
+        var smallImage = document.getElementById('smallImage');
+        smallImage.style.display = 'block';
+         
+        // Show the captured photo.
+        smallImage.src = imageURI;
+    },
+    
+    _onFail: function(message) {
+        alert('Failed! Error: ' + message);
+    }
 }
 
+// GENERAL UI CALLBACKS
+// ===================
+
+function afterShowSignin(e) 
+{
+
+	//window.localStorage.setItem("last_email_signin","alior101@gmail.com" );
+	//window.localStorage.setItem("last_password_signin", "kellogskellogs");
+
+	console.log(e.view);
+
+	//var auth_token = window.localStorage.getItem("auth_token");
+	console.log("auth_token = "+ _serverApi.auth_token);
+	if (_serverApi.auth_token != null )
+	{
+	    console.log("(auth_token != null )");
+	    app.navigate("views/homeView.html"); 
+	}
+	else if (user_data!=null)
+	{
+	     document.getElementById("email-signin").value = user_data['user']['email']; // window.localStorage.getItem("last_email_signin");
+	     document.getElementById("password-signin").value = user_data['user']['password']; //window.localStorage.getItem("last_password_signin");
+	}
+}
+
+   
+function closeViewSignIn() 
+{
+	app.showLoading();
+
+	var email = $("#email-signin").val();
+	var password = $("#password-signin").val();
+
+	user_data = {user: {email: email, password: password} }
+	user_data_str = JSON.stringify(user_data);
+	window.localStorage.setItem("user_data",user_data_str);
+
+	_serverApi.sign_in({
+	    data: user_data,
+	    success: function(response)  {
+		app.hideLoading();
+		app.navigate("views/homeView.html");
+		//app.navigate("#start");
+		console.log('navigate: ' + _serverApi.auth_token);
+	    },
+	    error: function(errorThrown)  {
+		app.hideLoading();
+		alert('Sign in failed');
+		app.navigate("#login");
+	    }
+	});
+}
+
+ 
+
+    function closeViewSignUp() 
+    {
+        var firstname = $("#firstname").val();
+        var lastname = $("#lastname").val();
+        var phone = $("#phone").val();
+        var credit_card_number = $("#credit_card_number").val();
+        var id_card_number = $("#id_card_number").val();
+        var email = $("#email-signup").val();
+        var password = $("#password-signup").val();
+        
+        app.showLoading();
+
+        //TODO: add phone and credit_card_number to server, add as accessible attr
+        user_data = {user: {email: email, password: password, password_confirmation: password, firstname: firstname, lastname: lastname } } //, phone: phone, credit_card_number: credit_card_number, id_card_number: id_card_number
+        user_data_str = JSON.stringify(user_data);
+        window.localStorage.setItem("user_data",user_data_str);
+
+        _serverApi.sign_up({
+            data: user_data,
+            success: function(response)  {
+                app.hideLoading();
+                $("#modalview-signup").kendoMobileView("close");
+                app.navigate("views/homeView.html");
+            },
+            error: function(errorThrown)  {
+                app.hideLoading();
+                alert('Signup Failed');
+                app.navigate("#login");
+            }
+        });
+    }
+ 
+ 
+     function closeModalAddCarCancel() 
+    {
+        console.log("closeModalAddCarCancel");
+        $("#modalview-addCar").data("kendoMobileModalView").close();
+    }
+    
+    function chooseCarToDelete() 
+    {                 
+        console.log("chooseCarToDelete");
+	    $('#CarsListScroller').mobiscroll('show');
+        // ofer delete the car 
+		return false;
+	}
+    
+    function updateAccountDetails()
+    {
+        app.navigate("#view-update-account");
+    }
+    
+    function prefillAccountDetails()
+    {
+        
+            console.log("prefill");
+            // OFER - get account details from server or from local storage ?
+            document.getElementById("update_email-signin").value = user_data['user']['email']; // window.localStorage.getItem("last_email_signin");
+            document.getElementById("update_password-signin").value = user_data['user']['password']; // window.localStorage.getItem("last_password_signin");
+            document.getElementById("update_firstname").value = user_data['user']['firstname']; //window.localStorage.getItem("last_lastname");
+            document.getElementById("update_lastname").value = user_data['user']['lastname']; //window.localStorage.getItem("last_lastname");
+            document.getElementById("update_phone").value = user_data['user']['phone']; // window.localStorage.getItem("last_phone");
+            document.getElementById("update_credit_card_number").value = user_data['user']['credit_card_number']; // window.localStorage.getItem("last_credit_card_number");
+    }
+    
+        
+    function closeViewSignOut() 
+    {
+        console.log("closeViewSignOut");
+        _serverApi.sign_out();
+        app.navigate("#login");
+        //app.navigate("../index.html");
+    }
+       
 
 
+   function closeModalAddCar() 
+    {
+        console.log("closeModalAddCar");
+	app.showLoading();
+	var smallImage = document.getElementById('smallImage');
+	var registration = $('#license_registration_number').val();
+	var car_description = $('#car_description').val();
+	var car_image_data = smallImage.src;
+	var data = {car: {license_plate: registration, car_description: car_description, car_:car_image_data}};
+	_serverApi.add_cars({ data: data, 
+		success: function(response) {
+			console.log(response);
+			app.hideLoading();
+			$("#modalview-addCar").data("kendoMobileModalView").close();	
+		},
+		error: function(error) 
+		{
+			console.log(error);
+			app.hideLoading();
+		}});
+    }
+         
+ function settingsViewInit() 
+ {  
+
+    console.log("settingsViewInit");
+    $('#scanView').click(function() 
+    {
+			window.plugins.barcodeScanner.scan(function(result) {
+			alert("We got a barcode\n" +
+				  "Result: " + result.text + "\n" +
+				  "Format: " + result.format + "\n" +
+				  "Cancelled: " + result.cancelled);
+		}, function(error) {
+			alert("Scanning failed: " + error);
+		}
+		);
+
+		return false;
+	});
+
+
+ }
+    
 //////////////////////////
 // APP DAta             //
 //////////////////////////
@@ -64,31 +398,7 @@ var parkyAppData = function() {
 		starbucksLocs: {path:"http://www.starbucks.com/api/location.ashx?&features=&lat={LAT}&long={LONG}&limit={MAX}", verb:"GET"},
 		starbucksTest: {path:"scripts/testData/starbucksTest.json", verb:"GET"}
 	};
-    /*
-    _initialCars = [
-		{
-			"carID":"12",			
-            "carNumber":"1234567",
-			"registerDate":"2013/12/06",
-            "parkingActive":"1"
-		},
-        {
-			"carID":"412",			
-			"carNumber":"2132436",
-			"registerDate":"2014/10/16",
-            "parkingActive":"0"
-		}
-	];
-    
-	_announcements = [
-		{ title: "Holiday Drinks Are Here", description: "Enjoy your favorite holiday drinks, like Pumpkin Spice Lattes.", url: "images/holiday.png" },
-		{ title: "Register & Get Free Drinks", description: "Register any Jitterz card and start earning rewards like free drinks. Sign-up now.", url: "images/rewards.png" },
-		{ title: "Cheers to Another Year", description: "Raise a cup of bold and spicy Jitterz Anniversary Blend.", url: "images/cheers.png" },
-		{ title: "Hot Drinks Anytime", description: "Find and enjoy our, hot drinks anytime.", url: "images/hot-drink.png" },
-		{ title: "Friend and Love", description: "Get more for your friends.Get Love.", url: "images/love-friend.png" },
-		{ title: "Wide range of choice", description: "Raise a cup of bold and spicy Jitterz Anniversary Blend.", url: "images/best-coffee.png" }
-	];
-    */
+
 	_private = {
 		load: function(route, options) {
 			var path = route.path,
