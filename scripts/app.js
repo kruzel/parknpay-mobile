11b1b1af1545e2ec1b6e78b445b52d216d46c8c0
lior <alior101@gmail.com>
2013-07-20 21:04:56 +0300

    moved everything into one file html+js

--- scripts/app.js
+++ scripts/app.js
@@ -256,8 +256,10 @@ function settingsViewInit()
 {  
 
     console.log("settingsViewInit");
+    /*
     $('#scanView').click(function() 
     {
+    	
 	window.plugins.barcodeScanner.scan(function(result) {
 		alert("We got a barcode\n" +
 			  "Result: " + result.text + "\n" +
@@ -267,9 +269,11 @@ function settingsViewInit()
 			alert("Scanning failed: " + error);
 		}
 		);
-
+	
 		return false;
 	});
+   */
+	
 }
  
 function afterShowSignin(e) 
@@ -285,7 +289,7 @@ function afterShowSignin(e)
 	if (_serverApi.auth_token != null )
 	{
 	    console.log("(auth_token != null )");
-	    app.navigate("views/homeView.html"); 
+	    app.navigate("#home"); 
 	}
 	else if (user_data!=null)
 	{
@@ -310,7 +314,7 @@ function closeViewSignIn()
 	    data: user_data,
 	    success: function(response)  {
 		app.hideLoading();
-		app.navigate("views/homeView.html");
+		app.navigate("#home");
 		console.log('navigate: ' + _serverApi.auth_token);
 	    },
 	    error: function(errorThrown)  {
@@ -352,7 +356,7 @@ function cancellViewSignIn()
                 app.hideLoading();
                 $("#modalview-signup").kendoMobileView("close");
                 app.navigate("#modalview-addCar"); 
-                app.navigate("views/homeView.html");
+                app.navigate("#home");
             },
             error: function(errorThrown)  {
                 app.hideLoading();
@@ -665,3 +669,377 @@ var parkyAppData = function() {
 	};
 }
 */
+
+
+    var cars_data;
+    var regions_data;
+    var done_with_cars_from_server;
+    var done_with_regions_from_server;
+    
+    
+    function digitized() 
+    {
+		var dt = new Date();    // DATE() CONSTRUCTOR FOR CURRENT SYSTEM DATE AND TIME.
+		var lt = new Date(localStorage.getItem("ParkingStartTime"));
+		var t2 = dt.getTime();
+		var t1 = lt.getTime();
+		
+		var dys = parseInt((t2 - t1) / (24 * 3600 * 1000));
+		var hrs = parseInt((t2 - t1) / (3600 * 1000) % 24);
+		var min = parseInt((t2 - t1) / (60 * 1000) % 60);
+		var sec = parseInt((t2 - t1) / 1000 % 60);
+		
+		/*
+		var hrs = dt.getHours();
+		var min = dt.getMinutes();
+		var sec = dt.getSeconds();
+		*/
+		min = Ticking(min);
+		sec = Ticking(sec);
+		
+		//var el1 = document.getElementById('dc');
+		var el1 = document.getElementById('parking_selection_item-title');
+		
+		if (ParkingActive) {
+			el1.innerHTML = hrs + ":" + min + ":" + sec; 
+			//document.getElementById('dc_second').innerHTML = sec;
+			var rate = localStorage.getItem("chosen_region_rate");
+			document.getElementById('parking_selection_item-info').innerHTML = parseInt(sec * rate / 3600) + '$';   
+			//document.getElementById('cost').innerHTML = parseInt( sec * rate / 3600)+'$';        
+		}
+
+		var time;
+		
+		// THE ALL IMPORTANT PRE DEFINED JAVASCRIPT METHOD.
+		if (ParkingActive) {
+			time = setTimeout('digitized()', 1000);      
+		}
+	}
+	
+	function Ticking(ticVal) {
+		if (ticVal < 10) {
+			ticVal = "0" + ticVal;
+		}
+		return ticVal;
+	}
+	
+	function homeBeforShow(e) {
+		console.log(e.view);
+	}
+	
+    
+    function handle_parking_panel()
+    {
+        
+        var status = localStorage.getItem("ParkingActive");
+	if (status == 1)
+        {
+			ParkingActive = true;
+			digitized();
+			document.getElementById('parking_selection_item-title').innerHTML = 'Active';
+			return false;
+	}
+	else 
+        {
+			ParkingActive = false;
+			localStorage.setItem("ParkingActive", "0");
+			console.log('parking stoped');
+			document.getElementById('parking_selection_item-title').innerHTML = 'Start';
+			document.getElementById('parking_selection_item-info').innerHTML = '';
+			return false;  
+	}
+    } 
+    
+    function set_up_cars_scrollers()
+    {
+    
+	     	/* ===================== */
+	    	/* CAR SELECTION SCROLER */
+	    	/* ===================== */
+	    	
+           	var template = kendo.template($("#CarsListScrollerTemplate").html());        
+		var result = template({data:cars_data}); //Execute the template  
+        	$("#CarsListScrollerTemplateResults").html(result);
+		
+        	// do the same for the list of cars to e deleted - inside there is the tag #DelCarsListScroller which is being shown 
+        	// when the relvant view is navigated to 
+       		template = kendo.template($("#DelCarsListScrollerTemplate").html());        
+		result = template({data:cars_data}); //Execute the template  
+        	$("#DelCarsListScrollerTemplateResults").html(result);
+ 
+		/* this will put on the home screen the last chosen car*/
+		pid = localStorage.getItem("chosen_car");
+    
+		if ((pid != null) && (cars_data.length > 0) && (cars_data[pid] != 'undefined'))
+        	{
+        
+			document.getElementById("car_selection_item_title").innerHTML = cars_data[pid].registration;
+			document.getElementById("car_selection_item_description").innerHTML = cars_data[pid].Name;
+		}
+		else 
+       		{
+			document.getElementById("car_selection_item_title").innerHTML = "No Car"
+			document.getElementById("car_selection_item_description").innerHTML = "registered yet...";
+		}
+        
+		$('#CarsListScroller').mobiscroll().image({
+			theme: 'android',
+			display: 'modal',
+			mode: 'scroller',
+			labels: ['My Car'],
+			inputClass: 'i-txt',
+			setText: 'OK',
+			onSelect: function (v, inst) 
+			{
+			},
+			onChange: function (v, inst) 
+			{
+				if (v != 'undefined')
+				{
+					$("#car_selection_item_title").html(cars_data[v].registration);
+					$("#car_selection_item_description").html(cars_data[v].Name);
+					/* save the car selected in persistent storage */
+					localStorage.setItem("chosen_car", v);
+				}
+			},
+		});
+     
+		$('#DelCarsListScroller').mobiscroll().image({
+			theme: 'android',
+			display: 'inline',
+			mode: 'scroller',
+			labels: ['Car'],
+			inputClass: 'i-txt',
+			onSelect: function (v, inst) 
+			{
+			},
+			onChange: function (v, inst) 
+			{
+				if (v != 'undefined')
+				{
+					localStorage.setItem("chosen_car_to_delete", v);
+				}
+				
+			},
+		});	
+
+		
+		$('#car_selection').click(function() 
+        	{                 
+    			if (cars_data[pid] != null) 
+		    	{
+	    			$('#CarsListScroller').mobiscroll('setValue', [pid, cars_data[pid].registration , cars_data[pid].PictureUrl ], true, .2);    
+	    		}
+    	    		$('#CarsListScroller').mobiscroll('show');
+			return false;
+		});
+		
+    }
+    
+    function set_up_regions_scrollers()
+    {
+    
+		/* ======================== */
+		/* REGION SELECTION SCROLER */
+		/* ======================== */
+	
+		var regions_template = kendo.template($("#RegionsListScrollerTemplate").html());
+		result = regions_template(regions_data); //Execute the template
+ 
+ 		$("#RegionsListScrollerTemplateResults").html(result);
+		pid1 = localStorage.getItem("chosen_region_city");
+		pid2 = localStorage.getItem("chosen_region_suburb");
+		
+		$('#RegionsListScroller').mobiscroll().treelist({
+			theme: 'android',
+			display: 'modal',
+			mode: 'scroller',
+			labels: ['City', 'Suburb'],
+			inputClass: 'i-txt',
+			setText: 'OK',
+			onSelect: function (v, inst) {
+			},
+			onChange: function (v, inst) {
+				$("#region_selection_item_title").html(regions_data[inst.temp[0]].Suburb[inst.temp[1]].sub + ", " + regions_data[inst.temp[0]].City);
+				localStorage.setItem("chosen_region_city", inst.temp[0]);
+				localStorage.setItem("chosen_region_suburb", inst.temp[1]);
+				localStorage.setItem("chosen_region_rate", regions_data[inst.temp[0]].Suburb[inst.temp[1]].rate);
+				$("#region_selection_item_description").html(regions_data[inst.temp[0]].Suburb[inst.temp[1]].rate + "$/h");
+			},
+		}); 
+		
+		if ((pid1 != null) && (pid2 != null)) {
+			$("#region_selection_item_title").html(regions_data[pid1].Suburb[pid2].sub + ", " + regions_data[pid1].City);
+			//$("#region_selection_item_title").html(pid2 + ", " + pid1);
+			$('#RegionsListScroller').mobiscroll('setValue', [pid1, pid2 ], true, .2);   
+			$("#region_selection_item_description").html(regions_data[pid1].Suburb[pid2].rate + "$/h");
+		}
+
+        
+        $('#region_selection').click(function() 
+        {
+    		var result = regions_template(regions_data); //Execute the template
+    		// console.log(result);
+    		$("#RegionsListScrollerTemplateResults").html(result);
+    		$('#RegionsListScroller').mobiscroll('show');
+    		return false;
+    	});
+    }
+    function update_regions_and_rates_from_server()
+    {
+    		// TODO - replace that with call to server
+    		regions_data = [
+			{
+				ID: "0", City: "Sydney", Suburb:[
+					{ID: "0", sub: "Badgerys Creek", info:"", rate: 0.1},
+					{ID: "1", sub: "Balgowlah", info:"", rate: 0.2},
+					{ID: "2", sub: "Balgowlah Heights", info:"", rate: 0.3},
+					{ID: "3", sub: "Balmain", info:"", rate: 0.4},
+					{ID: "4", sub: "Balmain East", info:"", rate: 0.5},
+					{ID: "5", sub: "Banksia", info:"", rate: 0.6},
+					{ID: "6", sub: "Banksmeadow", info:"", rate: 0.7},
+					{ID: "7", sub: "Bankstown",info:"", rate: 0.8}
+				]
+			},{
+				ID: "1", City: "Newcastle", Suburb:[
+					{ID: "0", sub: "Adamstown", info:"", rate: 0.15},
+					{ID: "1", sub: "Adamstown Heights",info:"", rate: 0.25},
+					{ID: "2", sub: "Bar Beach", info:"", rate: 0.35},
+					{ID: "3", sub: "Beresfield", info:"", rate: 0.45},
+					{ID: "4", sub: "Birmingham Gardens", info:"", rate: 0.55},
+					{ID: "5", sub: "Black Hill", info:"", rate: 0.65},
+					{ID: "6", sub: "Banksmeadow",info:"", rate: 0.75},
+					{ID: "7", sub: "Callaghan", info:"", rate: 0.85}
+				]
+			}
+		];
+		set_up_regions_scrollers();
+		done_with_regions_from_server  = true;
+		if (done_with_cars_from_server)
+		{
+			app.hideLoading();
+		}				
+
+
+    }
+    function update_cars_list_from_server()
+    {
+   
+    		_serverApi.get_cars({ 
+			success: function(response) {
+				console.log(response);
+				_cars_data = response; //JSON.stringify(response);
+				
+				var jsonObj = [];
+				var i = 0;
+				for (i= 0; i< response.length; i++)
+				{
+					if ( response[i].archive == false ) // keep only undelete cars
+					{
+						jsonObj.push({
+							"index": i,
+							"ID": response[i].id,
+							"registration": response[i].license_plate, 
+							"PictureUrl": "http://"+response[i].image_url, 
+							"Name": response[i].car_description});
+					}
+				}
+				localStorage.setItem('cars_data', JSON.stringify(jsonObj) );
+				cars_data = JSON.parse(localStorage.getItem('cars_data'));
+				if (cars_data == null)
+				{
+					cars_data = [];
+				}
+				set_up_cars_scrollers();
+				done_with_cars_from_server  = true;
+				if (done_with_regions_from_server)
+				{
+					app.hideLoading();
+				}				
+			},
+			error: function(error) 
+			{
+				console.log(error);
+				app.hideLoading();
+			}});
+
+    }
+    
+    // this is called ONLY ONCE before the firt time the home screen is shown
+    function init_home_view()  
+    {
+	 
+         	console.log("calling init_home_view");   
+         	
+		done_with_cars_from_server = false;
+		done_with_regions_from_server = false;
+  
+       		app.showLoading();
+       		
+       		update_cars_list_from_server();
+       		update_regions_and_rates_from_server();
+       		
+          	handle_parking_panel();
+		
+
+      
+    	// ======================== 
+    	// PARKING SELECTION SCROLER 
+    	// ======================== 
+    
+        
+    	$('#parking_selection').click(function() 
+        {
+    	
+    		//debugger;
+    	    	status = localStorage.getItem("ParkingActive");
+            	pid = 	cars_data[localStorage.getItem("chosen_car")].ID;
+    		pid1 = localStorage.getItem("chosen_region_city");
+    		pid2 = localStorage.getItem("chosen_region_suburb");
+    		if ((pid == null) || (pid1 == null) || (pid2 == null)) 
+            {
+    			alert("Please set the car and the location first");
+    			return false;
+    		}
+    		
+    		status = localStorage.getItem("ParkingActive");
+    		if ((status == null) || (status == 0)) 
+            {
+    			var dt = new Date(); 
+    			ParkingActive = true;
+    			localStorage.setItem("ParkingActive", "1");
+    			localStorage.setItem("ParkingStartTime", dt);
+    			digitized();
+    			console.log('parking started');
+    			document.getElementById('parking_selection_item-title').innerHTML = 'Starting...';
+    			return false;
+    		}
+    		else 
+            {
+    			ParkingActive = false;
+    			localStorage.setItem("ParkingActive", "0");
+    			console.log('parking stoped');
+    			document.getElementById('parking_selection_item-title').innerHTML = 'Start';
+    			document.getElementById('parking_selection_item-info').innerHTML = '';
+    			return false;  
+    		}
+    		
+    		if ((localStorage.getItem("ParkingActive") == undefined) || (localStorage.getItem("ParkingActive") == "0")) 
+            {
+    			document.getElementById('parking_selection_item-title').innerHTML = 'Start';
+    			// document.getElementById('dc_second').innerHTML = '';
+    			//document.getElementById('cost').innerHTML = '';
+    			ParkingActive = false;
+    		}
+    		else 
+            {
+    			ParkingActive = true;
+    		}
+            
+    		if (ParkingActive) 
+            {
+    			digitized();
+    		} 
+        });	
+	}
+	
